41ceb49396377b2cbabe5e9a56837974abfca7ea
Your Name <jigneshmakvana077@gmail.com>
2025-01-25 11:33:43 +0530

    add new project

--- src/Array/Projects.js
+++ src/Array/Projects.js
@@ -1,6 +1,6 @@
-import Youtube from '../assets/yt.png'
 import project1 from '../assets/project1.png'
 import project2 from '../assets/project2.png'
+import project3 from '../assets/project3.png'
 
 const Project = [
     {
@@ -8,8 +8,8 @@ const Project = [
         name: 'Streaming Website',
         category: 'system like youtube',
         paragraf: 'My streaming website allows users to upload, watch, and manage videos. Built with the MERN stack, it offers smooth streaming and features like subscriptions, likes, and comments for better engagement.',
-        github_link: '#',
-        live_demo: '#',
+        github_link: 'https://github.com/jignesh0703/YT-front-end',
+        live_demo: 'https://mern-stack-virid-ten.vercel.app',
         array: [
             'React.js',
             'Node.js',
@@ -19,12 +19,25 @@ const Project = [
         ]
     },
     {
+        imgs: project3,
+        name: 'Style Haus',
+        category: 'E-commerce Website',
+        paragraf: 'With a user-friendly interface, this E-commerce website makes it easy to find and purchase products anytime, anywhere.',
+        github_link: 'https://github.com/jignesh0703/e-commerce',
+        live_demo: 'https://e-commerce-jigs.netlify.app',
+        array: [
+            'React.js',
+            'Context',
+            'tailwind',
+            'javascript'
+        ]
+    },
+    {
         imgs: project1,
         name: 'Yumify',
         category: 'Online Food Booking',
         paragraf: 'Yumify is a Website that provide online food booking and payment systems .Built the complete site from scratch.',
-        github_link: '#',
-        live_demo: '#',
+        github_link: 'https://github.com/jignesh0703/Yumify',
         array: [
             'React.js',
             'tailwind',
--- src/Components/Laptop.jsx
+++ src/Components/Laptop.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Laptop = ({imgs,name,category,paragraf,github_link,live_demo,array}) => {
+const Laptop = ({ imgs, name, category, paragraf, github_link, live_demo, array }) => {
     return (
         <>
             <div className='border border-[#555555] px-8 rounded-[30px] pb-5 w-[20rem] sm:w-[35rem] md:w-[45rem] lg:w-[55rem] xl:w-[65rem] 2xl:w-[75rem]'>
-                <div className='flex flex-col md:flex-row md:mt-8 lg:mt-0'> 
+                <div className='flex flex-col md:flex-row md:mt-8 lg:mt-0'>
                     <div className='duration-300 transition flex lg:ml-[-2rem] xl:ml-0'>
                         <div className='flex w-[15rem] sm:w-[20rem] md:w-[15rem] lg:w-[25rem] xl:w-[25rem] 2xl:w-[30rem] ml-[1rem] sm:ml-[6rem] md:ml-0 overflow-hidden z-30 transition'>
                             <img src={imgs} alt="laptop_img" />
@@ -15,17 +15,20 @@ const Laptop = ({imgs,name,category,paragraf,github_link,live_demo,array}) => {
                         <h1 className='mt-1 font-bold'>({category})</h1>
                         <p className='mt-3 lg:w-[25rem] xl:w-[35rem]'>{paragraf}</p>
                         <div className='flex gap-x-2 sm:gap-x-4 gap-y-2 w-[18rem] sm:w-[30rem] md:w-[25rem] lg:w-[25rem] xl:w-[38rem] flex-wrap mt-4'>
-                        {
-                            array.map((item, index) => (
-                               <h1 className='border-black border rounded-[30px] py-1 text-[0.7rem] sm:text-base px-4' key={index}>#{item}</h1>
-                            ))
-                        }
+                            {
+                                array.map((item, index) => (
+                                    <h1 className='border-black border rounded-[30px] py-1 text-[0.7rem] sm:text-base px-4' key={index}>#{item}</h1>
+                                ))
+                            }
                         </div>
                     </div>
                 </div>
                 <div className='flex justify-center gap-4 items-center mt-4 sm:mt-6 lg:mt-0'>
                     <a href={github_link}><button className='border-[1px] border-[#555555] px-6 py-2 rounded-[30px] font-semibold text-[0.8rem] transition duration-200 hover:bg-[#555555] hover:text-white'>Github</button></a>
-                    <a href={live_demo}><button className='border-[1px] border-[#555555] px-6 py-2 rounded-[30px] font-semibold text-[0.8rem] transition duration-200 hover:bg-[#555555] hover:text-white'>Live Demo</button></a>
+                    {
+                        live_demo &&
+                        <a href={live_demo}><button className='border-[1px] border-[#555555] px-6 py-2 rounded-[30px] font-semibold text-[0.8rem] transition duration-200 hover:bg-[#555555] hover:text-white'>Live Demo</button></a>
+                    }
                 </div>
             </div>
         </>
